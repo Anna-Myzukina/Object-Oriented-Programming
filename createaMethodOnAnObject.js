a00b08673121e118467993f730f436e753a70add
Ann <anna.muzykina83@gmail.com>
2019-04-14 19:27:52 +0200

    Update createaMethodOnAnObject.js

--- createaMethodOnAnObject.js
+++ createaMethodOnAnObject.js
@@ -19,3 +19,14 @@ The next challenge will cover another way to do this.
 
 Using the dog object, give it a method called sayLegs. The method should return the sentence "This dog has 4 legs."*/
 
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function(){
+    return "This dog has " + dog.numLegs + " legs.";
+  }
+};
+
+dog.sayLegs();
+
