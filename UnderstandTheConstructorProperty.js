094913f3934f10ca15b86aaf09b866398540ed76
Ann <anna.muzykina83@gmail.com>
2019-04-15 11:18:06 +0200

    Update UnderstandTheConstructorProperty.js

--- UnderstandTheConstructorProperty.js
+++ UnderstandTheConstructorProperty.js
@@ -28,3 +28,22 @@ Write a joinDogFraternity function that takes a candidate parameter and, using t
 return true if the candidate is a Dog, otherwise return false.
 
 */
+
+function Dog(name) {
+  this.name = name;
+}
+
+// Add your code below this line
+function joinDogFraternity(candidate) {
+  if (candidate.constructor === Dog) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+
+
+
+
