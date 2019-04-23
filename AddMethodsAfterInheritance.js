6b1f59d8ee3369a4def4953ffc8f93fa172a7ef4
Ann <anna.muzykina83@gmail.com>
2019-04-23 11:24:25 +0200

    Update AddMethodsAfterInheritance.js

--- AddMethodsAfterInheritance.js
+++ AddMethodsAfterInheritance.js
@@ -29,3 +29,29 @@ Then add a bark() method to the Dog object so that beagle can both eat() and bar
 The bark() method should print "Woof!" to the console.
 
 */
+
+function Animal() { }
+Animal.prototype.eat = function() 
+{ console.log("nom nom nom"); };
+
+function Dog() { }
+
+// Add your code below this line
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function() {
+    console.log("Woof!");
+}; 
+
+
+
+// Add your code above this line
+
+let beagle = new Dog();
+
+beagle.eat(); // Should print "nom nom nom"
+beagle.bark(); // Should print "Woof!"
+
+
+
