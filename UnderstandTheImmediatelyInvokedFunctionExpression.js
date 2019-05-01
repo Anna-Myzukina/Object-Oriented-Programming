26c2ef7d979dfcdb9d997fad66efdf44fe00e5d5
Ann <anna.muzykina83@gmail.com>
2019-05-01 18:14:36 +0200

    Update UnderstandTheImmediatelyInvokedFunctionExpression.js

--- UnderstandTheImmediatelyInvokedFunctionExpression.js
+++ UnderstandTheImmediatelyInvokedFunctionExpression.js
@@ -14,3 +14,8 @@ This pattern is known as an immediately invoked function expression or IIFE.
 Rewrite the function makeNest and remove its call so instead it's an anonymous immediately invoked function expression (IIFE).
 
 */
+
+
+(function () {
+  console.log("A cozy nest is ready");
+})();
